c67c3be4204f9b1857616391c746b8371f2b1958
LiorM30 <liormor2005@gmail.com>
2023-01-22 16:19:36 +0200

    added sender UDP TCP

--- RIONet/ga2230net/src/main/java/org/ga2230net/socket_handlers/SenderSocket.java
+++ RIONet/ga2230net/src/main/java/org/ga2230net/socket_handlers/SenderSocket.java
@@ -1,6 +1,6 @@
 package org.ga2230net.socket_handlers;
 
-import java.net.Socket;
+import java.net.*;
 
 import java.io.IOException;
 import java.io.DataOutputStream;
@@ -8,18 +8,34 @@ import java.io.DataOutputStream;
 import org.ga2230net.packets.Packet;
 
 public class SenderSocket {
-    private Socket sock;
+    private Socket sock = null;
+    private DatagramSocket datagramSocket = null;
     private DataOutputStream outStream;
 
-    public SenderSocket() {}
+    private final String ip;
+    private final int port;
+    private final SocketType socketType;
+    private final InetAddress listenerAddress;
+
+    public SenderSocket(String ip, int port, SocketType socketType) throws IOException {
+        this.ip = ip;
+        this.port = port;
+        this.socketType = socketType;
+
+        this.listenerAddress = InetAddress.getByName(ip);
+
+        if (socketType == SocketType.UDP) {
+            datagramSocket = new DatagramSocket(port);
+        }
+    }
 
     /**
-     * connects the sender to a listener on the given ip and port
-     *
-     * @param ip   the ip of the listener to connect to
-     * @param port the port of the listener to connect to
+     * connects the TCP sender to a TCP listener on the given ip and port
+     * @throws IOException if there was a problem while connecting to listener
+     * @throws SocketHandlerException if the sender is set as UDP
      */
-    public void connect(String ip, int port) throws IOException {
+    public void connect() throws IOException, SocketHandlerException {
+        if (socketType == SocketType.UDP) throw new SocketHandlerException("a UDP sender cannot connect to listener");
         sock = new Socket(ip, port);
         outStream = new DataOutputStream(sock.getOutputStream());
     }
@@ -28,10 +44,21 @@ public class SenderSocket {
      * sends a packet to the listener
      *
      * @param packet the packet to send
+     * @throws IOException if there was a problem while sending the packet
+     * @throws SocketHandlerException if the sender is of TCP and a connection to a listener wasn't established
      */
     public void sendPacket(Packet packet) throws IOException, SocketHandlerException {
-        if (outStream == null)
-            throw new SocketHandlerException("Must first astablish a connection to listener before sending!");
-        outStream.write(packet.serialize());
+        if (socketType == SocketType.TCP) {
+            if (outStream == null)
+                throw new SocketHandlerException("Must first establish a connection to listener before sending!");
+            outStream.write(packet.serialize());
+        } else if (socketType == SocketType.UDP) {
+            byte[] ser = packet.serialize();
+            DatagramPacket pack = new DatagramPacket(
+                ser, ser.length,
+                listenerAddress, port
+            );
+            datagramSocket.send(pack);
+        }
     }
 }

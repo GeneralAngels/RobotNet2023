7657ae7d6695d587cfc3535df065237090560b6c
LiorM30 <liormor2005@gmail.com>
2023-02-14 16:19:15 +0200

    repackaged libs

--- PyNet/setup.py
+++ PyNet/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='ga2230net',
-    version='1.0',
+    version='1.2',
     description='A networking library made for python side networks',
     url="https://github.com/GeneralAngels/RobotNet2023",
-    author=["Lior" , "keysar"],
+    author=["Lior", "keysar"],
     packages=find_packages(),
     install_requires=[
         'PYYAML',
